85bdea18066224e679a6217e029f98b2dc17e83f
Soenke Huster <me@eknoes.de>
2015-06-16 00:18:38 +0200

    removed double

--- Direction.java
+++
@@ -1,8 +0,0 @@
-package de.eknoes.snake;
-
-/**
- * Created by soenke on 13.06.15.
- */
-public enum Direction {
-    TOP, BOTTOM, LEFT, RIGHT
-}
--- MainImpl.java
+++
@@ -1,7 +0,0 @@
-package de.eknoes.snake;
-
-/**
- * Created by soenke on 15.06.15.
- */
-public class MainImpl extends Main {
-}
